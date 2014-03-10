0eb545deb944ce049fe21a8258c7d52ba021b5a9
Sam Blackman <sblackman@gmail.com>
2014-03-10 14:55:27 -0500

    How do I create a basic rails app?
    rails new HackerNews -d postgresql
    This creates a database that connects to a postgres server.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '0c59cfc69b0697e1a6de5acffcaa32d43a92ccd2caa2d8b7281838c436ac9985d684e08bcad82d87903fc194ec9679a231d365b78224d562a4eeab38e6767d6e'
