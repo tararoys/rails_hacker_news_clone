92ad4abff87c8ae9fb4948a52a32db2ff046ec51
Sam Blackman <sblackman@gmail.com>
2014-03-10 15:35:41 -0500

    How to I add users and authentication to my application?
    Use devise. Set it up following this tutorial:
    http://guides.railsgirls.com/devise/

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+#Adding user authentication with devise
+
+gem 'devise'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 HackerNews::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Devise tells me to ensure that I have devined a default url options.
+  #I don't know what this does, and I am doing it anyway.
+   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
