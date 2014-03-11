dc5b54f5f5bda1009434edac721a4fdcb2839b69
Tara Roys <tlroys@gmail.com>
2014-03-10 21:05:02 -0500

    How do I see if I am passing in a password?
    Now I know this is a bad test, because all it's actually testing is if I passed a string called password in to the user. Huzzah for no validations!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,11 @@ describe User do
     expect(tara.email).to eq("tara@tara.com") 
   end
 
+  it "should have a password" do
+    tara = User.new(email: "tara@tara.com", password: "password")
+    expect(tara.password).to eq("password")
+  end 
+
+
 
 end
