ad0c870f22055b15c61003e35776fec230ea56e7
Tara Roys <tlroys@gmail.com>
2014-03-10 21:03:27 -0500

    How do I test if a user has an email?
    I'm pretty sure this is a bad test, since I don't think validations are run until you try to insert into the database.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe User do
+  after do
+    User.delete_all
+  end
+
+  it "should have an email" do
+    tara = User.new(email:"tara@tara.com")
+    expect(tara.email).to eq("tara@tara.com") 
+  end
 
 
 end
